cdb1195dceef33976e011ba0cfc6e481ad13c79c
Andrew Taggart <andrew.taggart@sendgrid.com>
2016-02-07 11:01:22 -0700

    Add move validation

--- parse.go
+++ parse.go
@@ -27,3 +27,56 @@ func Parse(puzzle []byte) (Board, error) {
 	}
 	return b, nil
 }
+
+func (b *Board) PushMove(i, j, value int) bool {
+	return b.isLegalMove(i, j, value)
+}
+
+func (b *Board) isLegalMove(i, j, value int) bool {
+	if i < 0 || i > 8 || j < 0 || j > 8 {
+		return false
+	}
+	if b.b[i][j] != 0 {
+		return false
+	}
+	if value < 1 || value > 9 {
+		return false
+	}
+	return b.uniqueInRow(i, value) && b.uniqueInCol(j, value) && b.uniqueInBox(i, j, value)
+}
+
+func (b *Board) uniqueInRow(i, value int) bool {
+	for _, v := range b.b[i] {
+		if v == value {
+			return false
+		}
+	}
+	return true
+
+}
+
+func (b *Board) uniqueInCol(j, value int) bool {
+	for _, row := range b.b {
+		v := row[j]
+		if v == value {
+			return false
+		}
+	}
+	return true
+}
+
+func box(i, j int) (int, int) {
+	return i / 3 * 3, j / 3 * 3
+}
+
+func (b *Board) uniqueInBox(i, j, value int) bool {
+	minX, minY := box(i, j)
+	for _, row := range b.b[minX : minX+3] {
+		for _, v := range row[minY : minY+3] {
+			if v == value {
+				return false
+			}
+		}
+	}
+	return true
+}
--- parse_test.go
+++ parse_test.go
@@ -29,3 +29,53 @@ func TestParse(t *testing.T) {
 		t.Fatalf("Board position is incorrect: got %d, wanted %d", v, 5)
 	}
 }
+
+type move struct {
+	i, j, v int
+	success bool
+}
+
+var exampleMoves = []move{
+	{1, 1, 8, false},
+	{0, 0, 10, false},
+	{0, 0, -1, false},
+	{0, 0, 0, false},
+	{-1, -1, 1, false},
+	{-1, 0, 1, false},
+	{0, -1, 1, false},
+	{9, 9, 1, false},
+	{9, 0, 1, false},
+	{0, 9, 1, false},
+	{0, 0, 6, false}, // value exists in row
+	{0, 0, 7, false}, // value exists in column
+	{0, 0, 5, false}, // value exists in box
+	{8, 8, 9, false}, // value exists in box
+	{8, 8, 2, true},
+	{5, 4, 5, true},
+	{5, 4, 7, false},
+	{5, 4, 9, false},
+	{5, 4, 1, false},
+	{1, 4, 7, true},
+	{1, 4, 6, false},
+	{1, 4, 2, false},
+	{1, 4, 5, false},
+	{0, 3, 5, true},
+}
+
+func TestBoardPushMove(t *testing.T) {
+	puzzle, err := ioutil.ReadFile("example.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, example := range exampleMoves {
+		board, err := Parse(puzzle)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		success := board.PushMove(example.i, example.j, example.v)
+		if success != example.success {
+			t.Fatalf("Expected success of (%d, %d, %d) to be %t", example.i, example.j, example.v, example.success)
+		}
+	}
+}
